3ad4e5cf34540ce83d25d470b8b69b46047fafff
starfrost013 <mario64crashed@gmail.com>
2025-12-31 13:12:22 +0000

    fix logging name of script file when script file cannot be opened

--- src/core/script/gpu_script_parser.c
+++ src/core/script/gpu_script_parser.c
@@ -133,7 +133,7 @@ void Script_Run()
 
 	if (!script_file)
 	{
-		Logging_Write(log_level_error, "Couldn't open script file %s\n");
+		Logging_Write(log_level_error, "Couldn't open script file %s\n", script_file);
 		exit(7);
 	}
 
